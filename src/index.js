9fa6c264e7f87bbc9f503be5ae0017f7ceb8ab37
Raul Macêdo De Sena Neto <raulneto213@gmail.com>
2026-05-15 10:37:47 -0400

    Atualiza portfolio com nova pagina

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// This portfolio is served as a standalone HTML page from public/index.html.
+// Keep the bundled React entrypoint empty so Create React App builds without
+// trying to mount over the static page.
